cd5ed69b4d7bdf08051e7bc66c6a70f556ce6839
vishwas99 <40636941+vishwas99@users.noreply.github.com>
2025-03-21 19:59:22 +0530

    Initial commit for 3.4.3 Spring boot Project

---
+++ src/main/java/com/chipIn/ChipIn/ChipInApplication.java
@@ -0,0 +1,13 @@
+package com.chipIn.ChipIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChipInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChipInApplication.class, args);
+	}
+
+}
